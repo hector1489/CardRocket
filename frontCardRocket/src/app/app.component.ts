4ac49a3e557e98e1a446a7b6cb5f0acbd9a36755
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-03-23 15:42:30 -0300

    [preparing]

--- frontCardRocket/src/app/app.component.ts
+++ frontCardRocket/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardsComponent } from './cards/cards.component';
 import { FooterComponent } from './footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, CardsComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontCardRocket';
--- frontCardRocket/src/app/cards/cards.component.ts
+++ frontCardRocket/src/app/cards/cards.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './cards.component.html',
-  styleUrl: './cards.component.css'
+  styleUrl: './cards.component.scss'
 })
 export class CardsComponent {
 
