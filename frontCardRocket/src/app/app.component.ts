a534f5aa864d1a40479b2eeac778aeb6d1746d46
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-03-27 15:12:21 -0300

    [fix] routes

--- frontCardRocket/src/app/app.component.ts
+++ frontCardRocket/src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardsComponent } from './cards/cards.component';
 import { FooterComponent } from './footer/footer.component';
 import { CardsWCssComponent } from './cards-w-css/cards-w-css.component';
 import { IntroComponent } from './intro/intro.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterLink,
     NavbarComponent,
     IntroComponent,
     CardsComponent,
     CardsWCssComponent,
+    ContactComponent,
     FooterComponent
   ],
   templateUrl: './app.component.html',
